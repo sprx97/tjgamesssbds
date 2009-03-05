f9e3e61d840778a5dd47201bd438efc7a2932c63
Daniel Johnson <ComputerDruid@gmail.com>
2009-03-04 21:42:11 -0500

    Reformatted to use .h file. WILL NOT COMPILE: more needed for that

---
+++ SSBDS/include/projectiles.h
@@ -0,0 +1,22 @@
+#ifdef PROJECTILES_ON
+#ifndef PROJECTILES_H //prevents errors with chained inclusion of headers
+static const int SHADOWBALL_SMALL = 0, SHADOWBALL_MEDIUM = 1, SHADOWBALL_LARGE = 2, FINALCUTTER = 3, FIREBALL = 4, FLUDDWATER = 5, IKESWORD = 6;
+// shortcuts for projectiles
+class Projectile {
+	public:
+		double x, y, dx, dy;
+		double maxy, miny; // for bouncing
+		Hitbox hit;
+		int length;
+		int time;
+		int TYPE;
+		int num;
+		int owner;
+		Stage mystage;
+		Projectile(double xpos, double ypos, double xchange, double ychange, int l, int t, int ob, Hitbox h, Stage mine);
+		void act();
+		void removeSelf();
+		Fighter* checkHits(Fighter* other);
+};
+#endif
+#endif
