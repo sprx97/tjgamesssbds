96bd5885cc96866bd4f75446bdcd81e78e78b541
Jeremy Vercillo <icelax1992@comcast.net>
2009-03-18 21:01:42 -0400

    Revert "Fixed top screen menus"
    This reverts commit f332ce5b3362fa27463aac30d6dcd97caf3e8ced.

--- SSBDS/source/main.cpp
+++ SSBDS/source/main.cpp
@@ -1116,6 +1116,8 @@ void cameraOptions() {
 } // edit camera options
 void gameOptions() {
 	openGif(SUB_SCREEN, "/SSBDS_Files/gifs/menubg.gif");
+	openGif(MAIN_SCREEN, "/SSBDS_Files/gifs/menu.gif");
+	//put main menu on top screen while in a submenu
 	
 #ifdef MP3_ON
 	AS_MP3StreamPlay("/SSBDS_Files/music/Menu.mp3");
@@ -1213,8 +1215,6 @@ void gameOptions() {
 void initOptions() {
 	openGif(SUB_SCREEN, "/SSBDS_Files/gifs/menubg.gif");
 	// opens gif background. no need to reinit, just loads over the old gif for this screen.
-	openGif(MAIN_SCREEN, "/SSBDS_Files/gifs/menu.gif");
-	// puts main menu on top screen while in submenus
 
 #ifdef MP3_ON
 	AS_MP3StreamPlay("/SSBDS_Files/music/Menu.mp3");
@@ -1293,10 +1293,9 @@ double distance(int x1, int y1, int x2, int y2) {
 // (Even more) pre-game menus
 void initMainMenu() {
 	PA_Init8bitBg(SUB_SCREEN, 3);
-	PA_Init8bitBg(MAIN_SCREEN, 3);
 	openGif(SUB_SCREEN, "/SSBDS_Files/gifs/menu.gif");
 	// opens gif background. no need to reinit, just loads over the old gif for this screen.
-	openGif(MAIN_SCREEN, "/SSBDS_Files/gifs/title2.gif");
+	openGif(MAIN_SCREEN, "/SSBDS_Files/gifs/title.gif");
 	//put title screen on top screen when at main menu.
 
 #ifdef MP3_ON
@@ -1304,6 +1303,7 @@ void initMainMenu() {
 	// plays main menu music
 #endif
 	PA_ResetSpriteSysScreen(MAIN_SCREEN);
+	PA_ResetBgSysScreen(MAIN_SCREEN);
 
 	PA_InitText(MAIN_SCREEN, 0);
 	PA_SetTextCol(MAIN_SCREEN, 31, 31, 31);
