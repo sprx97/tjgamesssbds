25f924c15e1de68e9dfd11620b2dd3ddadac0ed1
Daniel Johnson <ComputerDruid@gmail.com>
2009-06-09 09:11:22 +0800

    more documentation
    Signed-off-by: Spartan97 <icelax1992@comcast.net>

--- SSBDS/include/effect.h
+++ SSBDS/include/effect.h
@@ -1,15 +1,16 @@
 #ifndef EFFECT_H
 #define EFFECT_H
 
-// shortcuts for visual effects
+/// Stores necessary information to display a visual effect onscreen for more than one frame
 class Effect {
 	public:
-		double x, y; // x and y pos of the effect
-		int mynum; // the reference number of the effect
-		int delay; // how long the effect waits before changing
-		int playernum; // which player the effect is created by
-		int type; // what effect type it is
-		Effect(double xpos, double ypos, int t, int pn, double scrollx, double scrolly); // creates a new effect
-		bool act(); // acts; called every frame
-}; // a visual effect
+		double x; ///< the x position of the effect
+		double y; ///< the y position of the effect
+		int mynum; ///< the reference number of the effect
+		int delay; ///< how long the effect waits before changing
+		int playernum; ///< which player the effect is created by
+		int type; ///< what type of effect it is
+		Effect(double xpos, double ypos, int t, int pn, double scrollx, double scrolly); ///< creates a new effect
+		bool act(); ///< acts; called every frame
+};
 #endif
