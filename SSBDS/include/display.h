643fad6b9ee1011af3b85500cd4acffec5f25194
Daniel Johnson <ComputerDruid@gmail.com>
2009-06-09 09:14:24 +0800

    comment display class
    Signed-off-by: Spartan97 <icelax1992@comcast.net>

--- SSBDS/include/display.h
+++ SSBDS/include/display.h
@@ -5,16 +5,18 @@
 #include "scoreboard.h"
 #include <vector>
 using std::vector;
+
+/// Keeps track of various aspects of the screen
 class Display{
 	public:
 		Display();
-		vector<Effect> effects;
+		vector<Effect> effects; ///< list of all the effects displayed onscreen
 		double scrollx;
 		double scrolly;
-		void addeffect(Effect e);
-		void deleffect(Effect e);
-		void updateEffects();
-		vector<Effect>* getEffects();
+		void addeffect(Effect e); ///< add a new effect to be displayed onscreen
+		void deleffect(Effect e); ///< remove an effect from the screen
+		void updateEffects(); ///< count down and remove old effects which have expired
+		vector<Effect>* getEffects(); ///< return the list of effects (effects)
 		Scoreboard *score;
 };
 #endif
